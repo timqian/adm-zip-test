1463a8c1bec3f2e504f3d8049031d7c9fa3c7664
timqian <timqian@t9t.io>
2021-02-05 11:16:30 +0800

    init

---
+++ test.js
@@ -0,0 +1,15 @@
+// const extract = require('extract-zip');
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+const fileContent = fs.readFileSync('./hello.sh', 'utf-8');
+const fileStat = fs.statSync('./hello.sh');
+
+const zip = new AdmZip();
+
+zip.addFile('./unzippedFolder/hello.sh', fileContent, null, fileStat.mode);
+// zip.addFile('./unzippedFolder/hello.sh', fileContent, null, 0o755);
+
+fs.writeFileSync('./a.zip', zip.toBuffer());
+// zip.writeZip('./a.zip')
+
+zip.extractAllTo('./')
